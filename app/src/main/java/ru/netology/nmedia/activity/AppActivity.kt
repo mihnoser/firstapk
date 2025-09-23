6c2c39e64b3c76422a9adbfeb498133b547f99c7
Sergey Mihno <mihnoser@yandex.ru>
2025-09-23 20:33:13 +0700

    dz Push

--- app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
@@ -1,10 +1,17 @@
 package ru.netology.nmedia.activity
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.messaging.FirebaseMessaging
 import ru.netology.nmedia.R
 import ru.netology.nmedia.databinding.ActivityAppBinding
 import ru.netology.nmedia.util.StringArg
@@ -42,6 +49,37 @@ class AppActivity : AppCompatActivity() {
 
     companion object{
         var Bundle.textArg: String? by StringArg
+    }
+
+    private fun requestNotificationsPermission() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+            return
+        }
+
+        val permission = Manifest.permission.POST_NOTIFICATIONS
+        if (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+
+        requestPermissions(arrayOf(permission), 1)
+    }
+
+    private fun checkGoogleApiAvailability() {
+        with(GoogleApiAvailability.getInstance()) {
+            val code = isGooglePlayServicesAvailable(this@AppActivity)
+            if (code == ConnectionResult.SUCCESS) {
+                return@with
+            }
+            if (isUserResolvableError(code)) {
+                getErrorDialog(this@AppActivity, code, 9000)?.show()
+                return
+            }
+            Toast.makeText(this@AppActivity, R.string.google_play_unavailable, Toast.LENGTH_LONG).show()
+        }
+
+        FirebaseMessaging.getInstance().token.addOnSuccessListener {
+            println(it)
+        }
     }
 
 }
---
+++ app/src/main/java/ru/netology/nmedia/services/FCMService.kt
@@ -0,0 +1,109 @@
+package ru.netology.nmedia.services
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Notification
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.google.gson.Gson
+import ru.netology.nmedia.R
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationManagerCompat
+import kotlin.random.Random
+
+
+class FCMService : FirebaseMessagingService() {
+
+    private val action = "action"
+    private val content = "content"
+    private val gson = Gson()
+    private val channelId = "remote"
+
+    override fun onCreate() {
+        super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_remote_name)
+            val descriptionText = getString(R.string.channel_remote_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+            }
+            val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        message.data[action]?.let { action ->
+            when (Action.valueOf(action)) {
+                Action.LIKE -> hundleLike(gson.fromJson(message.data[content], Like::class.java))
+                Action.NEW_POST -> handleAddPost(gson.fromJson(message.data[content], AddPost::class.java))
+                Action.ERROR -> println("ERROR_PUSH")
+            }
+        }
+    }
+
+    private fun hundleLike(content: Like) {
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(
+                getString(R.string.notification_user_liked, content.userName, content.postAuthor))
+
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        notify(notification)
+    }
+
+    @SuppressLint("StringFormatInvalid")
+    private fun handleAddPost(content: AddPost) {
+
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(
+                getString(R.string.notification_new_post, content.postAuthor))
+            .setContentText(content.postTopic)
+            .setStyle(NotificationCompat.BigTextStyle()
+                .bigText(content.postText))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        notify(notification)
+    }
+
+    private fun notify(notification: Notification) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU ||
+            checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED
+        ) {
+            NotificationManagerCompat.from(this).notify(Random.nextInt(100_000), notification)
+        }
+    }
+
+    override fun onNewToken(token: String) {
+        Log.i("fcm token", token)
+    }
+
+    enum class Action {
+        LIKE, NEW_POST, ERROR;
+    }
+
+    data class Like(
+        val userId: Int,
+        val userName: String,
+        val postId: Int,
+        val postAuthor: String,
+    )
+
+    data class AddPost(
+        val userId: Int,
+        val postAuthor: String,
+        val postText: String,
+        val postTopic: String
+    )
+
+}
